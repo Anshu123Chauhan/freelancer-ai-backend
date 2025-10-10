a0982083b6d6a2c39087415e513e8a772cbc29d5
Srishti Sharma <106140991+shri5445@users.noreply.github.com>
2025-10-10 12:07:06 +0530

    update the order Admin / seller

--- src/dashboard/order/routes.js
+++ src/dashboard/order/routes.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import {getOrders,getOrderById} from './controller/index.js'
+import {getOrders,getOrderById, updateOrder} from './controller/index.js'
 const router = express.Router();
 
 router.get('/',getOrders);
 router.get('/:id',getOrderById);
+router.put('/:id',updateOrder);
 
 
 export default router
